4141b8968304e6dbb37724f8fae85b3717ad42c6
vibey19 <jdadhaniya6@gmail.com>
2024-02-20 20:29:05 +0530

    add user creation

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -9,6 +9,7 @@ import { Input } from "@/components/ui/input"
 import { SignupValidation } from '@/lib/validation'
 import Loader from '@/components/shared/Loader'
 import { Link } from 'react-router-dom'
+import { createUserAccount } from '@/lib/appwrite/api'
 
 const SignupForm = () => {
   const isLoading = false;
@@ -26,7 +27,9 @@ const SignupForm = () => {
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // const newUser = await createUserAccount(values);
+    const newUser = await createUserAccount(values);
+
+    console.log(newUser);
   }
 
   return (
